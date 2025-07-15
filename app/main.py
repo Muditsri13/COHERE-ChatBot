60834f7d617e36cb4b978c9668579e88bd00fee0
Muditsri13 <muditmrbigby@gmail.com>
2025-07-15 15:53:35 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from .routes import router
-
+from app.routes import router
 
 app = FastAPI()
-app.include_router(router)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+app.include_router(router)
 
 @app.get("/")
 def home(request: Request):
